68cf3d6603199e085e9e89bcf3ddc7bea449d4af
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-12-19 23:57:50 +0200

    створення

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,54 @@
+// import PropTypes from 'prop-types';
+// import { MutatingDots } from 'react-loader-spinner';
+// import styles from './Loader.module.css';
+
+// const Loader = ({ size = 100, color = '#3498db' }) => {
+//   return (
+//     <div className={styles.loaderContainer} aria-label="Loading...">
+//       <MutatingDots
+//         height={size}
+//         width={size}
+//         color={color}
+//         secondaryColor={color}
+//         radius={size / 8}
+//         ariaLabel="loading"
+//       />
+//     </div>
+//   );
+// };
+
+// Loader.propTypes = {
+//   size: PropTypes.number,
+//   color: PropTypes.string,
+// };
+
+// export default Loader;
+
+
+// Loader.tsx
+import PropTypes from 'prop-types';
+import { MutatingDots } from 'react-loader-spinner';
+import styles from './Loader.module.css';
+import React from 'react';
+
+const Loader = React.memo(({ size = 100, color = '#3498db' }) => {
+  return (
+    <div className={styles.loaderContainer} aria-label="Loading...">
+      <MutatingDots
+        height={size}
+        width={size}
+        color={color}
+        secondaryColor={color}
+        radius={size / 8}
+        ariaLabel="loading"
+      />
+    </div>
+  );
+});
+
+Loader.propTypes = {
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+export default Loader;
